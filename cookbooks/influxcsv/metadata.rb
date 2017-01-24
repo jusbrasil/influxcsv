53d8ec926a0f2801bf0bf18a738ff77629ba3f08
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2017-01-24 11:15:45 -0300

    Hack to make it works with chef 12.x

--- cookbooks/influxcsv/metadata.rb
+++ cookbooks/influxcsv/metadata.rb
@@ -4,8 +4,9 @@ maintainer_email 'dev@jusbrasil.com.br'
 license          'All rights reserved'
 description      'Installs/Configures influxcsv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'git', '~> 0.10.0'
 depends 'supervisor', '~> 0.4.2'
 depends 'nginx', '~> 1.4.0'
+depends 'ohai', '< 4.0.0' # hack to make it works with chef 12.x
